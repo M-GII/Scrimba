3c9c285e328abb138c9a7a23e8a0d277d5afdba5
Manroj Gill <manrojgill@hotmail.com>
2025-07-31 01:05:05 -0600

    Learning basics of javascript

---
+++ Scrimba-Learning/JavaScript/Passenger App/index.js
@@ -0,0 +1,11 @@
+let count = 0 // How to set variables in JavaScript (they can be numbers,simple math operations + - / * , other variables and etc.)
+
+console.log(count) //How to show the value of a variable in JavaScript kinda like "print(count)"" in Python 
+                   // (has to be placed after variable initilization since JavaScript reads top to bottom)
+
+
+function increment() { //how to initilize functions in javascript
+    count = count +1
+    console.log(count) //How to show the value of a variable in JavaScript kinda like "print(count)"" in Python 
+                       // (has to be placed after variable initilization since JavaScript reads top to bottom)   
+}
