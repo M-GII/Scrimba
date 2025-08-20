bf021671fd7e22f5cf95e92fc14e69374a86d8da
Manroj Gill <manrojgill@hotmail.com>
2025-08-20 00:45:27 -0600

    Finished learning static pages with react

---
+++ Scrimba-Learning/React/ReactFacts/src/App.jsx
@@ -0,0 +1,11 @@
+import Main from "./components/Main.jsx"
+import Navbar from "./components/Navbar.jsx"
+
+export default function App() {
+    return (
+        <div className="content">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
