63c4c38c876f740b2270e87b2a207934671448e1
Manroj Gill <manrojgill@hotmail.com>
2025-09-08 13:48:51 -0600

    showcasing selected answer

--- Projects/React/Quizzical/src/Components/Questions.jsx
+++ Projects/React/Quizzical/src/Components/Questions.jsx
@@ -1,18 +1,24 @@
 import { useState, useEffect} from 'react'
 import he from "he"
 import { nanoid } from "nanoid"
+import { clsx } from "clsx"
+
 export default function Questions(){
     const [questions,setQuestions] = useState([])
+
+
+
     useEffect(() => {
         fetch("https://opentdb.com/api.php?amount=5")
             .then(res => res.json())
             .then(data => data.results)
             .then(result =>result.map(obj => {
-                const correct = { answer: he.decode(obj.correct_answer), isCorrect: true, id: nanoid() }
+                const correct = { answer: he.decode(obj.correct_answer), isCorrect: true, id: nanoid(), isClicked: false }
                 const incorrect = obj.incorrect_answers.map(ans => ({
                     answer: he.decode(ans),
                     isCorrect: false,
-                    id: nanoid()}))
+                    id: nanoid(),
+                    isClicked: false}))
                 const options = [...incorrect]
                 const randomIndex = Math.floor(Math.random() * (options.length + 1))
                 options.splice(randomIndex, 0, correct)
@@ -21,18 +27,22 @@ export default function Questions(){
                     ...obj,
                     question: he.decode(obj.question),
                     options,
+                    isSubmitted: false,
                     id: nanoid()}}))
             .then(finalData => setQuestions(finalData))
     }, []) 
     console.log(questions)
+
+
+
     const showQuestions = Array.isArray(questions) && questions.length > 0 ? questions.map((question,index) => {
       return( 
-        <section key = {index} className="Question">
+        <section key = {question.id} className="Question">
         <h2> {question.question} </h2>
         <section className='answer-buttons'>
             {question.options.map(answer =>{
                 return( 
-                <button className ="answer-btn" key ={answer.id}>{answer.answer}</button>
+                <button onClick = {() => makeSelection(question,answer.id)} className ={clsx("answer-btn" , answer.isClicked && "option-selected")} key ={answer.id}>{answer.answer}</button>
                 )
             })}
          </section>
@@ -42,6 +52,18 @@ export default function Questions(){
     ) 
     })
     : <h2>Please Reload Page </h2>
+
+    function makeSelection(questionAsked,id){
+        setQuestions(prev =>
+            prev.map(quest =>
+                quest.id === questionAsked.id
+                    ? {
+                        ...quest,
+                        options: quest.options.map(o => ({
+                        ...o,
+                        isClicked: o.id === id}))}
+                    : quest))}
+
     return (
         <section className="Questions">
             {showQuestions}
